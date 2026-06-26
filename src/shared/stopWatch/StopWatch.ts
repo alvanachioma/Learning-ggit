77b69cbe65f457f6f7c10dc6844bb5d32ed68312
Alfred Obialo <alfredcsdinc@gmail.com>
2026-06-26 07:47:32 +0100

    Timer Fixed

---
+++ src/shared/stopWatch/StopWatch.ts
@@ -0,0 +1,49 @@
+import {Component, computed, signal} from '@angular/core';
+
+@Component({
+  selector: 'StopWatch',
+  template: `
+    <div class="bg-red-500 text-[2.5rem] uppercase rounded-md m-3 font-extrabold italic text-white px-4 py-2 min-h-[50px] max-w-[350px] flex item-center justify-center" >
+      <span>{{elapsed()}}</span>
+    </div>
+  `
+})
+export class StopWatch {
+  private readonly CLOCK_SPEED  = 1000;
+  private readonly SEC  = 60;
+  private _internal_counter  = signal(1210);
+  protected second  = computed(() => {
+    return this._internal_counter() % this.SEC;
+  });
+  protected hour  = computed(() => {
+    return Math.floor(this._internal_counter() / (this.SEC * this.SEC));
+  });
+  protected minute  = computed(() => {
+    return Math.floor(this._internal_counter() / this.SEC);
+  });
+
+  protected elapsed  = computed(() => {
+    const hr = (this.hour() + "").length > 1 ? this.hour() : "0" + this.hour() ;
+    const min = (this.minute() + "").length > 1 ? this.minute() : "0" + this.minute() ;
+    const sec = (this.second() + "").length > 1 ? this.second() : "0" + this.second() ;
+    return `${hr} : ${min} : ${sec}`;
+  });
+
+  ngOnInit () {
+    this.startTimer();
+  }
+  protected startTimer(): void {
+
+    window.setInterval(() => {
+
+      if(this._internal_counter() <=0 ){
+        // Stop timer
+      }
+      else{
+        this._internal_counter.update(x => x - 1);
+      }
+      //console.log("INTERVAL TIME",this._internal_counter());
+    }, this.CLOCK_SPEED);
+  }
+
+}
