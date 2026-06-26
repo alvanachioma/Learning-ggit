b281b420a1556e8f22d3029ef54808addaf4efdb
Alfred Obialo <alfredcsdinc@gmail.com>
2026-06-26 12:18:15 +0100

    add stopWatch hour, minutes and seconds inputs

--- src/shared/stopWatch/StopWatch.ts
+++ src/shared/stopWatch/StopWatch.ts
@@ -1,4 +1,4 @@
-import {Component, computed, signal} from '@angular/core';
+import {Component, computed, signal, output, input, numberAttribute, effect, linkedSignal} from '@angular/core';
 
 @Component({
   selector: 'StopWatch',
@@ -11,37 +11,54 @@ import {Component, computed, signal} from '@angular/core';
 export class StopWatch {
   private readonly CLOCK_SPEED  = 1000;
   private readonly SEC  = 60;
-  private _internal_counter  = signal(9000);
 
-  protected second  = computed(() => {
+  hour = input(0, {transform :(x :number) => x > 23 ? 23  :x});
+  minute = input(0, {transform :(x :number) =>  x > 59 ? 59 : x});
+  second = input(0, {transform :(x :number) => x > 59 ? 59 : x});
+
+  onTimerElapsed = output();
+  onTimerStarted = output();
+  private _calculateCounterInSeconds(){
+    return this.hour() * 60 * 60 + this.minute() * 60 + this.second();
+  }
+  private _internal_counter  = linkedSignal(() => {
+    return this._calculateCounterInSeconds();
+  });
+
+  protected _second  = computed(() => {
     return this._internal_counter() % this.SEC;
   });
-  protected hour  = computed(() => {
+  protected _hour  = computed(() => {
     return Math.floor(this._internal_counter() / (this.SEC * this.SEC));
   });
-  protected minute  = computed(() => {
+  protected _minute  = computed(() => {
     let m = Math.floor(this._internal_counter() / this.SEC) ;
 
     //const m2 =
-    return m < 60 ? m : Math.floor(Math.abs(((this.hour() * this.SEC * this.SEC) - this._internal_counter()) / 60)) ;
+    return m < 60 ? m : Math.floor(Math.abs(((this._hour() * this.SEC * this.SEC) - this._internal_counter()) / 60)) ;
   });
 
   protected elapsed  = computed(() => {
-    const hr = (this.hour() + "").length > 1 ? this.hour() : "0" + this.hour() ;
-    const min = (this.minute() + "").length > 1 ? this.minute() : "0" + this.minute() ;
-    const sec = (this.second() + "").length > 1 ? this.second() : "0" + this.second() ;
+    const hr = (this._hour() + "").length > 1 ? this._hour() : "0" + this._hour() ;
+    const min = (this._minute() + "").length > 1 ? this._minute() : "0" + this._minute() ;
+    const sec = (this._second() + "").length > 1 ? this._second() : "0" + this._second() ;
     return `${hr} : ${min} : ${sec}`;
   });
+  constructor() {
+
+  }
 
   ngOnInit () {
     this.startTimer();
   }
   protected startTimer(): void {
-
+    this.onTimerStarted.emit();
     const clearInterval  = window.setInterval(() => {
       if(this._internal_counter() <=0 ){
         // Stop timer
         window.clearInterval(clearInterval);
+        this.onTimerElapsed.emit();
+        this._internal_counter.set(this._calculateCounterInSeconds());
       }
       else{
         this._internal_counter.update(x => x - 1);
