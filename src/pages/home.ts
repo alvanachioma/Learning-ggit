71cf136c954dc038a35215d39a4a26ceddcfd0a3
Alfred Obialo <alfredcsdinc@gmail.com>
2026-06-27 17:54:32 +0100

    reformat the home page

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -3,8 +3,6 @@ import {ExamQuestionService} from '../shared/examination-proj/services/exam-ques
 import {RouterLink} from '@angular/router';
 
 
-
-
 @Component({
   selector: 'mastering-git',
   imports: [
@@ -13,11 +11,20 @@ import {RouterLink} from '@angular/router';
   template: `
     <div class="">
       <div class="text-4xl mb-5">Select an Exam you wish to take</div>
-      @for(st of examsList; track $index){
+      @for (st of examsList; track $index) {
         <a class="px-6 py-4 ring-2 hover:ring-4 block lg:w-[60%]
         hover:ring-offset-orange-600 hover:bg-orange-800 hover:text-white duration-300
         ring-orange-400 bg-orange-50/30 text-orange-700 font-bold rounded-xl mb-4 " [routerLink]="['/exam']" [queryParams]="{id: st.id()}">
-          {{st.title()}}  <span class="text-green-700">@if(st.duration.hour > 0 ){<span>{{st.duration.hour}}hr </span>} @if(st.duration.minute > 0 ){<span>: {{st.duration.minute}}min</span>} @if(st.duration.second > 0 ){<span>: {{st.duration.second}}sec</span>}</span>
+          {{ st.title() }}  <span class="text-green-700">
+          @if (st.duration.hour > 0) {
+          <span>{{ st.duration.hour }}hr </span>
+        }
+          @if (st.duration.minute > 0) {
+            <span>: {{ st.duration.minute }}min</span>
+          }
+          @if (st.duration.second > 0) {
+            <span>: {{ st.duration.second }}sec</span>
+          }</span>
         </a>
       }
       <div class="bg-red-400 w-[200px] h-[200px]"></div>
