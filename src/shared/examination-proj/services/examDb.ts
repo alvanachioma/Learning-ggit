7f4c93520c83e2157809820d57327c5e3e6d9bd0
Alfred Obialo <alfredcsdinc@gmail.com>
2026-06-28 21:58:01 +0100

    Add ANSWER mapper

--- src/shared/examination-proj/services/examDb.ts
+++ src/shared/examination-proj/services/examDb.ts
@@ -1,17 +1,17 @@
-import {ExamSubject, QuizQuestion} from './exam-question-services';
-import {cSharpAdvanceExam} from './csharp-advanced-engineer';
-import {csharpBeginnersExam} from './csharp-beginner-intermediate';
-import {softwareArchExam} from './software-architect-exam';
-import {angularExam} from './angular-exam';
-import {advanceAngularExam} from './advanced-angular-developer';
-import {bibleExam} from './bible-study-beginner';
+import {AnswerKey, ExamSubject, QuizQuestion} from './exam-question-services';
+import {csharpAdvancedAnswers, cSharpAdvanceExam} from './csharp-advanced-engineer';
+import {csharpBeginnerAnswers, csharpBeginnersExam} from './csharp-beginner-intermediate';
+import {architectAnswers, softwareArchExam} from './software-architect-exam';
+import {angularExam, ngAnswers} from './angular-exam';
+import {advanceAngularExam, advancedAngularAnswers} from './advanced-angular-developer';
+import {bibleAnswers, bibleExam} from './bible-study-beginner';
 import {economicsExam} from './economicsExam';
-import {htmlExam} from './html-exam';
-import {cssExam} from './css-exam';
-import {jsExam} from './javascript-exam';
+import {htmlAnswers, htmlExam} from './html-exam';
+import {cssAnswers, cssExam} from './css-exam';
+import {jsAnswers, jsExam} from './javascript-exam';
 import {signal} from '@angular/core';
 
-export const examDb :ExamSubject[] = [
+export const examDb: ExamSubject[] = [
   economicsExam,
   htmlExam,
   cssExam,
@@ -24,9 +24,44 @@ export const examDb :ExamSubject[] = [
   bibleExam
 ]
 
+export const examAnswersDbMapper: {
+  examId: string, answers: AnswerKey[], hasAnswers?: boolean;
+} [] = [
+  {
+    examId: angularExam.id(), answers: ngAnswers, hasAnswers: true
+  },
+  {
+    examId: advanceAngularExam.id(), answers: advancedAngularAnswers, hasAnswers: true
+  },
+  {
+    examId: economicsExam.id(), answers: [], hasAnswers: false
+  },
+  {
+    examId: bibleExam.id(), answers: bibleAnswers, hasAnswers: true
+  },
+  {
+    examId: softwareArchExam.id(), answers: architectAnswers, hasAnswers: true
+  },
+  {
+    examId: csharpBeginnersExam.id(), answers: csharpBeginnerAnswers, hasAnswers: true
+  },
+  {
+    examId: cSharpAdvanceExam.id(), answers: csharpAdvancedAnswers, hasAnswers: true
+  },
+  {
+    examId: htmlExam.id(), answers: htmlAnswers, hasAnswers: true
+  },
+  {
+    examId: cssExam.id(), answers: cssAnswers, hasAnswers: true
+  },
+  {
+    examId: jsExam.id(), answers: jsAnswers, hasAnswers: true
+  },
+];
+
 export function randomizeArrayIndexes<T>(arr: T[]) {
   let currentIndex = arr.length;
-  let randomIndex = 0 ;// Math.floor(Math.random() * currentIndex);
+  let randomIndex = 0;// Math.floor(Math.random() * currentIndex);
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
@@ -36,20 +71,22 @@ export function randomizeArrayIndexes<T>(arr: T[]) {
   return arr;
 }
 
-export function randomizeQuestionsIfRequested<T>( questions: T[] ) {
-      return randomizeArrayIndexes(questions);
+export function randomizeQuestionsIfRequested<T>(questions: T[]) {
+  return randomizeArrayIndexes(questions);
 
 }
-function resetQuestions(questions: QuizQuestion[] ) {
+
+function resetQuestions(questions: QuizQuestion[]) {
   //questions.update(x => randomizeQuestionsIfRequested([...x]))
   questions.forEach(y => {
-      y.options.forEach(z => z.isSelected = false);
-    });
+    y.options.forEach(z => z.isSelected = false);
+  });
   return questions;
 }
-export function resetExamQuestions(exam : ExamSubject)  : ExamSubject{
+
+export function resetExamQuestions(exam: ExamSubject): ExamSubject {
   return {
     ...exam,
-    questions : signal(resetQuestions(exam.questions()))
+    questions: signal(resetQuestions(exam.questions()))
   }
 }
