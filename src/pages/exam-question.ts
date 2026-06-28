8a3b4d702b7b8f4ca6535a364dcefeea881b7862
Alfred Obialo <alfredcsdinc@gmail.com>
2026-06-28 21:58:11 +0100

    enhance UI

--- src/pages/exam-question.ts
+++ src/pages/exam-question.ts
@@ -29,42 +29,48 @@ import {ActivatedRoute, Router} from '@angular/router';
         @if (!hasTimerElapsed()) {
           @if (hasExamStarted() && !timerStopped()) {
             <div animate.leave="animate-slide-down" animate.enter="animate-slide-up" class="">
-              <h3 class="  2xl:text-3xl text-xl  font-normal mt-3 p-4 mb-4 border-b-green-400 border-b-2">{{ currentQuestion().title }}</h3>
-              @if(currentQuestion().isMultiOption){
-                <div class="text-lg bg-amber-100 p-4 text-center text-red-500 font-bold">This is a Multi-Option Question. Choose all options that apply</div>
-              }
+              <div class="flex flex-col justify-between gap-y-4">
+                <div class="min-h-[380px]">
+                  <h3 class="  2xl:text-3xl text-xl  font-normal mt-3 p-4 mb-4 border-b-green-400 border-b-2">{{ currentQuestion().title }}</h3>
+                  @if(currentQuestion().isMultiOption){
+                    <div class="text-lg bg-amber-100 p-4 text-center text-red-500 font-bold">This is a Multi-Option Question. Choose all options that apply</div>
+                  }
 
-              <div class="grid grid-cols-1 xl:grid-cols-2 xl:gap-4 gap-y-3">
+                  <div class="grid grid-cols-1 xl:grid-cols-2 xl:gap-4 gap-y-3">
 
-                @for (opt of currentQuestion().options; track opt.id) {
-                  <div
-                    (click)="markAsSelectedOption(opt)"
-                    [class]="(opt?.isSelected ?? false) ? selectedOptionClass() : ''"
-                    class="hover:bg-green-100 hover:ring-4 duration-300 hover:ring-green-400 bg-white ring-2 2xl:text-[1.5rem] text-xl cursor-pointer ring-gray-200 xl:p-4 px-2 py-2 rounded-md xl:rounded-2xl">{{ alphabetMap[$index] }} - {{ opt.text }}
+                    @for (opt of currentQuestion().options; track opt.id) {
+                      <div
+                        (click)="markAsSelectedOption(opt)"
+                        [class]="(opt?.isSelected ?? false) ? selectedOptionClass() : ''"
+                        class="hover:bg-green-100 hover:ring-4 duration-300 hover:ring-green-400 bg-white ring-2 2xl:text-[1.5rem] text-xl cursor-pointer ring-gray-200 xl:p-4 px-2 py-2 rounded-md xl:rounded-2xl">{{ alphabetMap[$index] }} - {{ opt.text }}
+                      </div>
+
+                    }
+
                   </div>
+                </div>
+                <div class="h-[150px] flex flex-col justify-end p-2 bg-green-200">
+                  <div class="flex justify-between items-center mt-6  ">
+                    <button (click)="gotoPreviousQuestion()"
+                            [disabled]="!canGoPrev()"
+                            class="disabled:bg-gray-300 disabled:ring-gray-300 disabled:text-gray-400  ring-2 rounded-lg cursor-pointer ring-zinc-600 duration-300 hover:ring-zinc-800 hover:bg-zinc-600 bg-zinc-800 text-white shadow px-6 py-2">
+                      Prev. Question
+                    </button>
+                    <button (click)="gotoNextQuestion()"
+                            [disabled]="!canGoNext()"
+                            class="disabled:bg-gray-300 disabled:ring-gray-300 disabled:text-gray-400 ring-2 rounded-lg cursor-pointer ring-zinc-600 duration-300 hover:ring-zinc-800 hover:bg-zinc-600 bg-zinc-800 text-white shadow px-6 py-2">
+                      Next Question
+                    </button>
+                  </div>
+                  <div class="text-center 2xl:mt-8 mt-2 lg:mt-4">
+                    <button (click)="confirmBeforeSubmit()"
+                            class="ring-2 disabled:bg-gray-300 disabled:text-gray-400 rounded-lg cursor-pointer not-[disabled]:ring-green-600 duration-300 hover:ring-green-800 hover:bg-green-600 bg-green-800 text-white shadow px-6 py-2 text-xl">
+                      Submit Exam
+                    </button>
 
-                }
-
+                  </div>
+                </div>
               </div>
-              <div class="flex justify-between items-center mt-6  ">
-                <button (click)="gotoPreviousQuestion()"
-                        class="ring-2 rounded-lg cursor-pointer ring-zinc-600 duration-300 hover:ring-zinc-800 hover:bg-zinc-600 bg-zinc-800 text-white shadow px-6 py-2">
-                  Prev. Question
-                </button>
-                <button (click)="gotoNextQuestion()"
-                        class="ring-2 rounded-lg cursor-pointer ring-zinc-600 duration-300 hover:ring-zinc-800 hover:bg-zinc-600 bg-zinc-800 text-white shadow px-6 py-2">
-                  Next Question
-                </button>
-              </div>
-              <div class="text-center 2xl:mt-8 mt-2 lg:mt-4">
-                <button (click)="confirmBeforeSubmit()"
-                        class="ring-2 disabled:bg-gray-300 disabled:text-gray-400 rounded-lg cursor-pointer not-[disabled]:ring-green-600 duration-300 hover:ring-green-800 hover:bg-green-600 bg-green-800 text-white shadow px-6 py-2 text-xl">
-                  Submit Exam
-                </button>
-
-              </div>
-
-
             </div>
           } @else if (timerStopped()) {
             <div class="mt-20 bg-orange-200 ring-4 ring-orange-400 rounded-xl p-6">
@@ -79,7 +85,7 @@ import {ActivatedRoute, Router} from '@angular/router';
               <h1 class="text-2xl text-gray-600 text-center">Exams will Start Soon!</h1>
               <div class="text-center mt-8">
                 <button (click)="startExam()"
-                        class="ring-2 disabled:bg-gray-300 disabled:text-gray-400 rounded-lg cursor-pointer not-[disabled]:ring-green-600 duration-300 hover:ring-green-800 hover:bg-green-600 bg-green-800 text-white shadow px-6 py-2 text-xl">
+                        class="ring-2 disabled:bg-gray-300 disabled:text-gray-400 rounded-lg cursor-pointer not-[disabled]:ring-green-600 duration-300 hover:ring-orange-800 hover:bg-orange-600 bg-orange-800 text-white shadow px-6 py-2 text-xl">
                   Start Exam
                 </button>
               </div>
@@ -103,7 +109,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 export class ExamQuestion {
 
   selectedOptionClass = signal("!bg-green-600 duration-300 text-white font-bold shadow hover:scale-105");
-  protected alphabetMap = "ABCDEFG";
+  protected alphabetMap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   questionIndex = signal(0);
   protected examQuestionService = inject(ExamQuestionService)
   private activeRoute = inject(ActivatedRoute);
@@ -138,7 +144,13 @@ export class ExamQuestion {
 
     })
   }
-
+  protected canGoNext = computed(() => {
+    console.log("QUESTION INDEX",this.questionIndex());
+    return this.questionIndex() < this.exam.questions().length-1;
+  });
+  protected canGoPrev = computed(() => {
+    return this.questionIndex() > 0;
+  });
   gotoNextQuestion() {
     this.questionIndex.update(x => {
       if (x < this.exam.questions().length - 1) return x + 1;
@@ -207,4 +219,7 @@ export class ExamQuestion {
   protected handleExamStarted() {
     this.hasExamStarted.set(true);
   }
+
+
+
 }
